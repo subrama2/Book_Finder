404354d2a595dc6fbb9d46e6f9b0e95cc28efeb2
burdhasp-uc <113551683+burdhasp-uc@users.noreply.github.com>
2023-10-10 12:23:18 -0400

    BookFinderController.java created

--- src/main/java/com/myplantdiary/enterprise/BookFinderController.java
+++ src/main/java/com/myplantdiary/enterprise/BookFinderController.java
@@ -1,7 +1,7 @@
-package com.myplantdiary.enterprise;
+package com.mybookfinder.enterprise;
 
-import com.myplantdiary.enterprise.dto.Person;
-import com.myplantdiary.enterprise.service.ISpecimenService;
+import com.mybookfinder.enterprise.dto.Person;
+import com.mybookfinder.enterprise.service.ISpecimenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,32 +10,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class PlantDiaryController {
+public class BookFinderController {
     /**
      * Handles the root (/) endpoint and return a start page
      * @return
      */
     @Autowired
-    ISpecimenService specimenService;
+    IBookService bookService;
     @RequestMapping("/")
     public String Index(Model model)
     {
-        Person person = new Person();
-        person.setDescription("Pawpaw fruit season");
-        person.setLatitude("39.74");
-        person.setLongitude("-84.51");
-        person.setSpecimenID("1003");
-        person.setPlantID(84);
-        model.addAttribute(person);
+        Person book = new Person();
+        model.addAttribute(book);
         return "start";
     }
 
-    @RequestMapping("/saveSpecimen")
-    public String saveSpecimen(Person person)
+    @RequestMapping("/saveBook")
+    public String saveBook(Person book)
     {
         try
         {
-            specimenService.save(person);
+            bookService.save(book);
         }
         catch (Exception e)
         {
@@ -47,33 +42,33 @@ public class PlantDiaryController {
     /**
      * Endpoint to fetch specimens
      */
-    @GetMapping("/specimen")
+    @GetMapping("/book")
     public ResponseEntity fetchAllSpecimens()
     {
         return new ResponseEntity(HttpStatus.OK);
     }
     /**
-     * Endpoint to fetch specimen by ID
+     * Endpoint to fetch book by ID
      */
-    @GetMapping("/specimen/{id}/")
+    @GetMapping("/book/{id}/")
     public ResponseEntity fetchSpecimenById(@PathVariable("id") String id)
     {
         return new ResponseEntity(HttpStatus.OK);
     }
 
     /**
-     * Endpoint to create specimen
+     * Endpoint to create book
      */
-@PostMapping(value = "/specimen", consumes = "application/json", produces = "application/json")
-    public Person createSpecimen(@RequestBody Person person)
+@PostMapping(value = "/book", consumes = "application/json", produces = "application/json")
+    public Person createSpecimen(@RequestBody Person book)
     {
-        return person;
+        return book;
     }
 
     /**
-     * Endpoint to delete specimen by ID
+     * Endpoint to delete book by ID
      */
-@DeleteMapping("/specimen/{id}")
+@DeleteMapping("/book/{id}")
     public ResponseEntity deleteSpecimen(@PathVariable("id") String id)
     {
         return new ResponseEntity(HttpStatus.OK);
