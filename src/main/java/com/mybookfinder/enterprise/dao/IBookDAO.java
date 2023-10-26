1272b4b5c97bf697c5ff024768ab647866664746
Bidhatri Amatya <abidhatri@gmail.com>
2023-10-26 13:27:22 -0400

    Created Interface PersonDAO

--- src/main/java/com/mybookfinder/enterprise/dao/IBookDAO.java
+++ src/main/java/com/mybookfinder/enterprise/dao/IBookDAO.java
@@ -1,6 +1,7 @@
 package com.mybookfinder.enterprise.dao;
 
 import com.mybookfinder.enterprise.dto.Book;
+
 public interface IBookDAO {
     Book save(Book book);
 }
---
+++ src/main/java/com/mybookfinder/enterprise/dao/IPersonDAO.java
@@ -0,0 +1,7 @@
+package com.mybookfinder.enterprise.dao;
+
+import com.mybookfinder.enterprise.dto.Person;
+
+public interface IPersonDAO {
+
+}
